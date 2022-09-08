3486f783094f5bd3ae2ae601a51c268d63fe7d90
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-08 22:32:24 +0000

    added c file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * programs to output a string of characters
+ * use the inbuilt function printf
+ * main: main program entry
+ * Return: always returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
