9ff02f4253dd06d8a5cd2ca325817e11a9d22477
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-08 21:54:34 +0000

    modified file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-char str[] = "Programming is like building a multilingual puzzle";
+/*string initialization*/
+char str[] = "\"Programming is like building a multilingual puzzle";
+/*displaying the string*/
 puts(str);
 return (0);
 
