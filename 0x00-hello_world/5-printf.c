56a0d3f971e516f14c30419799d35ad7e5817e7f
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-08 22:51:32 +0000

    modified 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * programs to output a string of characters
+ * main - entry point
+ * Description: programs to output a string of characters
  * use the inbuilt function printf
- * main: main program entry
  * Return: always returns 0
  */
 int main(void)
+/*main function: main program entry*/
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
