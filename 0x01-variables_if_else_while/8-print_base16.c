66c1ef647e7b4f4fd7b43b7629fc6b09e9229165
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-12 06:57:51 +0000

    added file 8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{	int i, ch;
+	
+	i = 0;
+	ch = 'a';
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (ch = 'a'; ch < 'g'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
