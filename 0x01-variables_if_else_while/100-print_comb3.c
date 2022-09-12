1ea8533aae5fcfe8ffa1eae2e8f7338fce920718
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-12 10:40:36 +0000

    modified the last line instruction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 		for (j = 0; j <= 9; j++)
-			if (i < j)
+			if (!(i == j) && i < j)
 			{
 			putchar(i + '0');
 			putchar(j + '0');
-			if (i < 8)
+			if (!(i == 8 && j == 9))
 			putchar(',');
 			putchar(' ');
 			}
