8d190583215e31d290387f8f10c5fb97cddf8a6c
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-14 10:20:12 +0000

    modified file 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,15 @@
 void print_alphabet_x10(void)
 {
 	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
+	int j = 0;
+	
+	while (j < 10)
 	{
-		_putchar(i);
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	j++;
 	}
-	_putchar('\n');
 }
