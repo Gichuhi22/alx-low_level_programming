dbfcbef1a3bc77eaaab9f152366276154ebccd16
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-14 18:52:14 +0000

    added the file 6-abs.c and its function prototype to main.h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - computes the absloute value of an integer
+ *
+ * @i: the [assed parameter value
+ *
+ *Return: returns the integer value i
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+	i = i / -1;
+	}
+	return (i);
+}
