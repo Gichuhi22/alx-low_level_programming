ed83f0b32abbd25f4a6f5ea45ff76f5271cebefd
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-14 09:40:20 +0000

    created file 2-print_alphabet_x10.c and modofied main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints lowercase alphabet 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
