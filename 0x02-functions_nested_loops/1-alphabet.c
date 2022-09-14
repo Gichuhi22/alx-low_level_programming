256830f35e4f6ebf7cfa6361ac9f263e1a91cd31
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-14 08:10:28 +0000

    created the file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ * main - function that prints lowercase alphabet
+ *
+ * Return: returns 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+}
+
+ int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
