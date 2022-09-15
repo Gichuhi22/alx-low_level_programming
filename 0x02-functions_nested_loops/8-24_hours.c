c796c6f0fbfb82256cb8c1380361dd570cac2d5b
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-15 02:57:01 +0000

    modified

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
-		{	
+		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(58);
@@ -22,5 +22,4 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
