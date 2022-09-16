9b120e0cf461b2232a125c1b29d730f8b6d2804c
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-16 15:27:50 +0000

    added file 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_line - draws a straight line
+ *@n: the passed parameter
+ *
+ *Return: null
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
+
+
