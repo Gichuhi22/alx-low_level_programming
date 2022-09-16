708aafb761cd62a79a14c2355a453aeccfb67978
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-16 13:13:15 +0000

    added files3-print_numbers.c and _putchar.c and function prototype in main.h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers 0-9
+ *
+ * Return: returns nothing
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
