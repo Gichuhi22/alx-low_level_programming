797754ff06f59fab467398865cc1b87fe793b479
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-16 01:56:32 +0000

    added new file 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - print fibonacci series upto 50
+ *
+ *Return: retuns 0
+ */
+int main(void)
+{
+	int i = 0;
+	long int next, a, b;
+
+	a = 0;
+	b = 1;
+
+	for (i = 0; i < 50; i++)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 49)
+		{
+			printf(",");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
