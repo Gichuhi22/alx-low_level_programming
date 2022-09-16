dd3ba1767e8b0e4dd05627524263044c2674bca0
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-16 14:49:56 +0000

    modified file 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,15 +12,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + 48);
-			}
-			else
+			if (j >= 10)
 			{
 				_putchar((j / 10) + 48);
-				_putchar((j % 10) + 48);
 			}
+				_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
