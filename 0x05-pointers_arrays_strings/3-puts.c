1cfa13e40885cf3efadd2dc6f0a85e8acce3e726
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-19 20:42:41 +0000

    added new file 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_puts - prints a string
+ *@str: a pointer to the string
+ *
+ * Return: null
+ */
+void _puts(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
