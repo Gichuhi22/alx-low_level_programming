9880b6aea870694367fccfca0b9caaecfc9d2fbb
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-20 10:23:29 +0000

    created the file 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcpy - cpoies one string to another
+ *@dest: destination string pointer
+ *@src: source string pointer
+ *Return: returns a charcter value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
