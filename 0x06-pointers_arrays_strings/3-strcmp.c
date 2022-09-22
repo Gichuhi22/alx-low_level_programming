bbe0f751476ceae25efd3838143559cf03967301
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-22 20:45:18 +0000

    modified file 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,20 +8,20 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, flag;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 		if (s1[i] > s2[i])
 		{
-			return (+15);
+			flag = +15;
 			break;
 		}
 		else if (s1[i] < s2[i])
 		{
-			return (-15);
+			flag = -15;
 			break;
 		}
 		else
-			return (0);
-	return (0);
+			flag = 0;
+	return (flag);
 }
