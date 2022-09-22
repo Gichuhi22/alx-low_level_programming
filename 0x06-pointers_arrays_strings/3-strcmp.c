516be39ef8934865e78b18f16aaa0c677261c591
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-22 20:41:48 +0000

    modified file 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,10 +14,12 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] > s2[i])
 		{
 			return (+15);
+			break;
 		}
 		else if (s1[i] < s2[i])
 		{
 			return (-15);
+			break;
 		}
 		else
 			return (0);
