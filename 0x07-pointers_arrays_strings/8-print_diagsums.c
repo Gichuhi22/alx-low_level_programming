9efa21ebd62708dddcd2d095b940bf17995905b4
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-26 21:21:53 +0000

    formatted file 8-print_diagsums to betty style

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,12 +14,12 @@ void print_diagsums(int *a, int size)
 
 	sum = 0;
 	sum1 = 0;
-	
+
 	for (i = 0; i < (size * size); i++)
 	{
 		if (i % (size + 1) == 0)
 			sum += a[i];
-		if (i % (size - 1) == 0 && i != 0 && i < size * size -1)
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
 			sum1 += a[i];
 	}
 	printf("%d, %d\n", sum, sum1);
