1dc410122cfed50029a0385dd4b5be7f639637f7
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-26 19:45:12 +0000

    added new files

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include"main.h"
+/**
+ * *_strstr - function that locates a substring
+ * @haystack: pointer to the first searched string
+ * @needle: pointer to search conditions
+ *
+ * Return: returns a pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] != needle[j])
+			{
+				return (0);
+			}
+			else
+			{
+				i++;
+				j++;
+				return (&haystack[i]);
+			}
+		}
+		
+	}
+	return (0);
+}
