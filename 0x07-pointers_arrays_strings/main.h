1af4141855ed0b0006e2eb6a12b13a34c434a14f
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-26 15:52:56 +0000

    added new files

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ char *_memset(char *, char, unsigned int );
 int _putchar(char);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
 
 
 
