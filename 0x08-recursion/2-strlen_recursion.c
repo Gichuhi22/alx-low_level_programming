c7fad24f8da9385cda7258b145bd3fa7cd56e445
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-28 23:59:24 +0000

    updated file 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,15 +9,15 @@
 
 int _strlen_recursion(char *s)
 {
-	static int count = 0;
+	static int count = 1;
 
 	if (*s != '\0')
 	{
-		count ++;
 		_strlen_recursion(s + 1);
+		++count;
 	}
 	else
-		return(0);
+		return (count = 0);
 
 	return (count);
 }
