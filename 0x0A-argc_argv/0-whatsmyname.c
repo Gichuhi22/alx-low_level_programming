36957bb4518e63c97dd276263d661fafcf1e8999
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-09-30 13:48:52 +0000

    added new files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints its name folled by new line
+ * @argc: store the number of arguments passed to
+ * the main function
+ * @argv: a string array of all passed arguments to main
+ *
+ * Return: returns 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,16 @@
+#COMMAND LINE ARGUMENTS
+
+
+#TASKS
+0-whatsmyname.c : program that prints its name followed by a new line.
+1-args.c : program that prints the number of arguments passed to it.
+
+
+
+#RESOURCES
+
+
+
+
+
+#THOUGHTS
