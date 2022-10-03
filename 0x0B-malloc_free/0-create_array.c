a1a5be495d2464df9d6951366c519899e3a04f5e
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-03 22:46:38 +0000

    Updated file 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,16 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
 	a = (char *)malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 		a[i] = c;
+	}
 
 	return (a);
 }
