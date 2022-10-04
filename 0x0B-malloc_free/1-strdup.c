97b496a2966f3f0f00c81b5bc95456642ca61e32
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-04 19:39:33 +0000

    updated 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,18 +18,24 @@ char *_strdup(char *str)
 	if (str == NULL)
 	{
 		return (NULL);
-		free(str);
 	}
 
 	str1 = (char *)malloc(sizeof(char) * length + 1);
-	if (!str)
+
+	if (str1 == NULL)
+	{
 		return (NULL);
+		free(str1);
+	}
 
 	for (i = 0; str[i] != '\0'; i++)
 		str1[i] = str[i];
 
 	if (str1 < str)
+	{
 		return (NULL);
+		free(str);
+	}
 
 	str1[length] = '\0';
 
