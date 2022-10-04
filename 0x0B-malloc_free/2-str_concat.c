1d952ff5c870fb98f23c1e8b1f4c06d6fb4c8e8c
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-04 18:37:39 +0000

    updated the file 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,9 @@ char *str_concat(char *s1, char *s2)
 	length1 = strlen(s1);
 	length2 = strlen(s2);
 	if (s1 == NULL)
-		s1 = "";
+		s1 = '\0';
 	if (s2 == NULL)
-		s2 = "";
+		s2 = '\0';
 
 	str = (char *)malloc((length1 + length2) * sizeof(char) + 1);
 
