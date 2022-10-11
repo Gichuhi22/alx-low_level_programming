e234be64d894d8be2c06edbd3c713af640d9cefe
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-11 23:33:23 +0000

    new

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,16 @@
+#STRUCTURES, TYPEDEF
+
+
+#TASKS
+0x0E- structures-typedef:define a new type 'struct dog'
+
+
+
+
+#RESOURCES
+
+
+
+
+
+#THOUGHTS
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+#include "main.h"
+/*
+ *struct dog - stores data entries for variable dog
+ *@name:first member
+ *@age: second entry
+ *@owner: third entry
+ *
+ *struct dog: sores the data of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
