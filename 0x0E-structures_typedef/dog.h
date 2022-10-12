36164147df622c07d5be07cc09d6431397a5ac96
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-12 02:17:17 +0000

    created the file 2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-#include "main.h"
+#include <stddef.h>
+#include<stdio.h>
 /*
  *struct dog - stores data entries for variable dog
  *@name:first member
@@ -18,4 +19,5 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif /* DOG_H */
