f9f62f36bd28af6ebea10291061766772b8f21d0
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-13 16:41:05 +0000

    modified the code on 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,13 +15,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *ptr;
 
 	if (name == NULL || age < 0 || owner == NULL)
+	{
 		return (NULL);
+		exit(1);
+	}
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 	{
 		free(ptr);
 		return (NULL);
+		exit(1);
 	}
 
 	ptr->name = malloc(sizeof(char) * (strlen(name) + 1));
@@ -29,6 +33,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		free(ptr->name);
 		return (NULL);
+		exit(1);
 	}
 
 	ptr->owner = malloc(sizeof(char) * (strlen(owner) + 1));
@@ -36,6 +41,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		free(ptr->owner);
 		return (NULL);
+		exit(1);
 	}
 
 	strcpy(ptr->name, name);
