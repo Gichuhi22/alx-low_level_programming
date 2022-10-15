3cbc895c32a470fd359109bfc391d03baf92ca39
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-15 12:50:16 +0000

    modified the file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,27 +7,35 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	char *s, *str;
-	int i = 0, num_args = 0, j = 0;
+	char *s, *str = "cifs";
+	int num_args, j, i = 0, c = 0;
 
-	str = "cifs";
 	num_args = strlen(format);
 	va_start(args, format);
 	while (i < num_args)
 	{
+		j = 0;
+		while (str[j])
+		{
+			if (format[i] == str[j] && c)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(args, int));
+				printf("%c", va_arg(args, int)), c = 1;
 				break;
 			case 'i':
-				printf("%d", va_arg(args, int));
+				printf("%d", va_arg(args, int)), c = 1;
 				break;
 			case 'f':
-				printf("%f", va_arg(args, double));
+				printf("%f", va_arg(args, double)), c = 1;
 				break;
 			case 's':
-				s = va_arg(args, char*);
+				s = va_arg(args, char*), c = 1;
 				if (s == NULL)
 				{
 					printf("(nil)");
@@ -35,15 +43,7 @@ void print_all(const char * const format, ...)
 				}
 				printf("%s", s);
 				break;
-		}
-		while (str[j] != '\0')
-		{
-			if (format[i] == str[j] && i < num_args - 1)
-			{
-				printf(", ");
-			} j++;
 		} i++;
 	}
-	printf("\n");
-	va_end(args);
+	printf("\n"), va_end(args);
 }
