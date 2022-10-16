02818050c2d714f7d2599e25ea67b12e695cb05e
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-16 09:57:18 +0000

    modified

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,5 +6,26 @@
  */
 int main(void)
 {
+	int i, j, isprime, x;
+	long int num = 612852475143;
 
+	for (i = 2; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			isprime = 1;
+			for (j = 2; j <= i / 2; j++)
+			{
+				if (i % j == 0)
+				{
+					isprime = 0;
+					break;
+				}
+			}
+			if (isprime == 1)
+				x = i;
+		}
+	}
+	printf("%d\n", x);
+	return (0);
 }
