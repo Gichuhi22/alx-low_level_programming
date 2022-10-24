30342df56b06401cbb6f1ea7bd468c4bbc778caa
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-24 23:18:25 +0000

    betty updated

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,8 +10,9 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *curr;
-	list_t* new_node = malloc(sizeof(list_t));
-	if(new_node ==NULL)
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
