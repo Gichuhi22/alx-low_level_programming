43f4cb76540d169d4eaa9db108af9660888956a4
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-25 19:32:16 +0000

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include"lists.h"
+/**
+ * printmefirst - print before main
+ */
+
+void printmefirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
