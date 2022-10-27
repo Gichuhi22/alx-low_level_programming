b38dc352470689c69350fa9a4d67a719ef72989b
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-27 19:35:47 +0000

    modified

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	new_node->n = n;
-	new_node->next = NULL;
+	if(*head == NULL && idx != 0)
+		return (NULL);
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
 
 	temp = *head;
 	if (temp)
@@ -31,10 +37,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			{
 				temp = temp->next;
 				count++;
-				if (temp->next == NULL || idx == 0)
-				{
-					return (NULL);
-				}
 			}
 			else
 			{
