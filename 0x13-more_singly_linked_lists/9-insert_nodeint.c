6e460dad70e9e20d73e0eb7279fb29daa8230637
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-27 19:22:04 +0000

    modified

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			{
 				temp = temp->next;
 				count++;
-				if (temp->next == NULL)
+				if (temp->next == NULL || idx == 0)
 				{
 					return (NULL);
 					break;
