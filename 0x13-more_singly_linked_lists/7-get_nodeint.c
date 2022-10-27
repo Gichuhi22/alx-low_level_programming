9aa718dc457905102aa13878bc2992ba396d3ed8
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-27 15:59:41 +0000

    new files

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,6 +18,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		count++;
 		if (count == index)
 			break;
+		if ((temp->next == NULL) && (count != index))
+			return (NULL);
 	}
 	return (temp);
 }
