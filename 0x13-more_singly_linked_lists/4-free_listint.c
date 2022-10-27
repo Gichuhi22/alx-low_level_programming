ea2f2dbfdf100bb84bde72614a4cef68857eedc3
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-27 11:37:43 +0000

    added new files

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include"lists.h"
+
+/**
+ * free_listint - frees an entire list
+ * @head: pointer to first element of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+	
+	temp = head;
+	while (temp->next != NULL)
+	{
+		head = head->next;
+		temp->next = NULL;
+		free(temp);
+		temp = head;
+	}
+	head = NULL;
+	temp = NULL;
+}
