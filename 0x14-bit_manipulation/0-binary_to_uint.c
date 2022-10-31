d007bf4d113d57cb1f0c030f5114296d536b3ddf
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-31 18:06:55 +0000

    new files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * num_pow - returns exponential result
+ * @base: base number
+ * @exp: exponent
+ * Return: exponential result
+ */
+
+unsigned int num_pow(int base, int exp)
+{
+	unsigned int result = 1;
+
+	if (exp == 0)
+		return (1);
+
+	while (exp != 0)
+	{
+		result = result * base;
+		exp--;
+	}
+	return (result);
+}
+
+/**
+ * binary_to_uint - converts binary to unsigned integer
+ * @b: pointer to binary string
+ * Return: converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i, length;
+	unsigned int num = 0;
+
+	length = strlen(b);
+
+	if (b)
+	{
+	for (i = length - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+			num += num_pow(2, ((length - 1) - i));
+		else if (b[i] == '0')
+			num += 0;
+		else
+			return (0);
+	}
+	}
+	else
+		return (0);
+
+	return (num);
+}
+
