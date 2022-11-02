601be1407abb05d8bbd04679a0a219d859ce0a53
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-11-02 21:05:38 +0000

    added one file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: string to write to the file
+ * Return: 1 on success or -1 on failure
+ */
+
+int create_file(const char *filename, char*text_content)
+{
+	int fd, i;
+
+	if (!filename)
+		return (-1);
+
+	if (access(filename, F_OK)== 0)
+	{
+		fd = open(filename ,O_TRUNC);
+	}
+	else
+	{
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	}
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		i = write(fd, text_content, (strlen(text_content) + 1));
+	
+	if (i == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
