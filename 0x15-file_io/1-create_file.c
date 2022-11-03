81d299742f6361ec9b8f8c97116d796feffd04d6
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-11-03 10:53:10 +0000

    modified code on 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,18 @@
 #include "main.h"
+#include<stdbool.h>
+
+bool file_exists(const char *filename)
+{
+	FILE *fp = fopen(filename, "r");
+	bool is_exist = false;
+
+	if (fp)
+	{
+		is_exist = true;
+		fclose(fp);
+	}
+	return (is_exist);
+}
 
 /**
  * create_file - creates a file
@@ -9,12 +23,12 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd, i, close_err;
 
 	if (!filename)
 		return (-1);
 
-	if (access(filename, F_OK) == 0)
+	if (file_exists(filename))
 	{
 		fd = open(filename, O_TRUNC);
 	}
@@ -32,7 +46,9 @@ int create_file(const char *filename, char *text_content)
 	if (i == -1)
 		return (-1);
 
-	close(fd);
+	close_err = close(fd);
+	if (close_err == -1)
+		return (-1);
 
 	return (1);
 }
