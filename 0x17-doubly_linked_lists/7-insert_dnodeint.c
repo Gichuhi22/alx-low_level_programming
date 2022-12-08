4c9088507814e29d34c95e3370ec634182a07cf7
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-12-08 17:17:05 +0000

    modified code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,6 +20,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	newnode->n = n;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	if (*h)
 	{
 		while (count != (idx - 1))
@@ -27,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			temp = temp->next;
 			count++;
 			if (temp->next == NULL)
-				return (NULL);
+				return (add_dnodeint_end(h, n));
 		}
 		newnode->next = temp->next;
 		newnode->prev = temp;
