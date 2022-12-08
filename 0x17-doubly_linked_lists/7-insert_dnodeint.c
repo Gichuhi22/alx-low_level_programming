d330ca304da80500f1c772f328d9c43b131bcc97
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-12-08 16:44:01 +0000

    added new file

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,39 @@
+#include"lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a node at a given index
+ * @h: pointer to first node
+ * @idx: index to insert node
+ * @n: integer member value of node
+ * Return: returns address of new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newnode, *temp;
+	unsigned int count = 0;
+
+	temp = *h;
+	newnode = malloc(sizeof(dlistint_t));
+	if (!newnode)
+	{
+		free(newnode);
+		printf("Failed to allocate memory\n");
+		return (NULL);
+	}
+	newnode->n = n;
+	if (h)
+	{
+		while (count != (idx - 1))
+		{
+			temp = temp->next;
+			count++;
+		}
+		newnode->next = temp->next;
+		newnode->prev = temp;
+		temp->next = newnode;
+		newnode->next->prev = newnode;
+	}
+	else
+		return (NULL);
+
+	return (newnode);
+}
